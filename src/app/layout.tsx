7e473224f21e23c47364b195b947db00cba5074f
Rosenfeld99 <124917070+Rosenfeld99@users.noreply.github.com>
2024-08-07 21:27:46 +0300

    adding the first design home page, and complate the auth with clerk

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,13 @@
-import type { Metadata } from "next";
+// src/app/layout.tsx
+"use client";
+
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
+import { ClerkProvider } from "@clerk/nextjs";
+import { metadata } from "./metadata"; // Import metadata
 
 const inter = Inter({ subsets: ["latin"] });
-
-export const metadata: Metadata = {
-  title: "Social Media",
-  description: "Social media app built with Next.js",
-};
 
 export default function RootLayout({
   children,
@@ -16,15 +15,21 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
-        <div className=" w-full bg-white px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
-          <Navbar />
-        </div>
-        <div className=" bg-slate-100 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
-          {children}
-        </div>
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <head>
+          <title>{metadata.title}</title>
+          <meta name="description" content={metadata.description} />
+        </head>
+        <body className={inter.className}>
+          <div className="w-full bg-white px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
+            <Navbar />
+          </div>
+          <div className="bg-slate-100 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
+            {children}
+          </div>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
